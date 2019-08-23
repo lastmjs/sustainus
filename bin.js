78a328f305fda61175c75f266f6c5fd98994fed5
Jordan Last <jordan.michael.last@gmail.com>
2019-08-22 20:43:19 -0600

    try fkill instead of fkill-cli

--- bin.js
+++ bin.js
@@ -1,34 +1,32 @@
 #!/usr/bin/env node
 
-const spawn = require('child_process').spawn;
-const pathToApp = require.resolve('sustainus/app.js');
-const pathToSustainus = pathToApp.replace('/app.js', '');
+(async () => {
 
-console.log('pathToApp', pathToApp);
-console.log('pathToSustainus', pathToSustainus);
+    const spawn = require('child_process').spawn;
+    const pathToApp = require.resolve('sustainus/app.js');
+    const pathToSustainus = pathToApp.replace('/app.js', '');
+    const fkill = require('fkill');
 
-const killProcess = spawn('node_modules/.bin/fkill', ['SUSTAINUS_MAIN_PROCESS']);
-
-killProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
-
-killProcess.stderr.on('data', (data) => {
-    console.log(data.toString());
-});
-
-const childProcess = spawn('node_modules/.bin/electron', [pathToApp], {
-    cwd: pathToSustainus
-});
-// const childProcess = spawn('electron', [pathToApp], {
-//     stdio: 'ignore',
-//     detached: true
-// }).unref();
-
-childProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
-
-childProcess.stderr.on('data', (data) => {
-    console.log(data.toString());
-})
+    console.log('pathToApp', pathToApp);
+    console.log('pathToSustainus', pathToSustainus);
+    
+    await fkill('SUSTAINUS_MAIN_PROCESS', {
+        silent: true
+    });
+        
+    const childProcess = spawn('node_modules/.bin/electron', [pathToApp], {
+        cwd: pathToSustainus
+    });
+    // const childProcess = spawn('electron', [pathToApp], {
+    //     stdio: 'ignore',
+    //     detached: true
+    // }).unref();
+    
+    childProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+    
+    childProcess.stderr.on('data', (data) => {
+        console.log(data.toString());
+    })
+})();
