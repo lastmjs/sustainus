e3b23d7d6cd79cc8dc5d69ea0a27bd3b63d73938
Jordan Last <jordan.michael.last@gmail.com>
2019-08-22 19:36:50 -0600

    test setting the working directory of the child process

--- bin.js
+++ bin.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 const spawn = require('child_process').spawn;
-const pathToApp = require.resolve('sustainus/app.js');
+const pathToSustainus = require.resolve('sustainus');
+const pathToApp = `${pathToSustainus}/app.js`;
 console.log('pathToApp', pathToApp);
 const childProcess = spawn('electron', [pathToApp], {
+    cwd: pathToSustainus
 });
 // const childProcess = spawn('electron', [pathToApp], {
 //     stdio: 'ignore',
